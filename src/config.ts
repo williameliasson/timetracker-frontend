a98db54f6a12399fc140bed717c0f88e6b0fa5db
williameliasson <williameliasson@users.noreply.github.com>
2026-05-06 18:47:06 +0200

    basic fetching of sessions onf indexpage

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,22 @@
-export const API_URL = "https://api.time.williameliasson.com/api";
-// export const API_URL = "http://localhost:8080/api"
+// export const API_URL = "https://api.time.williameliasson.com/api";
+export const API_URL = "http://localhost:8080/api"
 
 export type FormData = {
     username: string,
     password: string,
+}
+
+export type Session = {
+    // private String id;
+    // private Instant startTime;
+    // private Instant endTime;
+
+    // private String category;
+
+    id: string,
+    startTime: string,
+    endTime: string | null,
+    category: string
 }
 
 export const EMPTY_FORM: FormData = {username: "", password: ""}
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState, type ReactNode } from "react"
+import { API_URL, type Session } from "../config";
 
 function IndexPage() {
+  const [sessions, setSessions] = useState<Session[]>([]);
+  useEffect(() => {
+    //fetch and store in sessions stat var
+    fetch(API_URL + "/me/sessions", {
+      method: "GET",
+      credentials: "include"
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      setSessions(data);
+    })
+  }, [])
+  const allSessions = sessions.map(item => {
+    return <p key={item.id}> {item.category}: {item.startTime} - {item.endTime || ''}</p>
+  })
   return (
-    <div>IndexPage</div>
+    <div>
+      <h1>Index</h1>
+      <div>
+        {allSessions}
+      </div>
+    </div>
   )
 }
 
