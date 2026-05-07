a41f10eee9fedfe71cbb8a494049c06777ace1cf
williameliasson <williameliasson@users.noreply.github.com>
2026-05-07 11:36:15 +0200

    function to handle submit on new category name

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,5 +1,5 @@
 import { useState, type ChangeEvent } from "react"
-import type { Category } from "../config"
+import { API_URL, type Category } from "../config"
 
 type CategoryCardProps = {
     category: Category
@@ -11,9 +11,20 @@ function CategoryCard({category}: CategoryCardProps) {
         const {value} = e.target;
         setCategoryName(value);
     }
+
+    const handleSubmit = () => {
+        fetch(API_URL + "/me/categories/" + category.id, {
+            method: "PATCH",
+            body: JSON.stringify({
+                name: categoryName
+            }),
+            credentials: "include"
+        })
+    }
   return (
     <div className="category-card" key={category.id}>
         <input name="categoryName" id={category.id} value={categoryName} onChange={handleChange}></input>
+        <button onClick={handleSubmit}>✅</button>
     </div>
   )
 }
