e561a681bb45bd8128e7612649c07a7349163b2d
williameliasson <williameliasson@users.noreply.github.com>
2026-05-07 17:45:19 +0200

    small fix

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://api.time.williameliasson.com/api";
-export const API_URL = "http://localhost:8080/api"
+export const API_URL = "https://api.time.williameliasson.com/api";
+// export const API_URL = "http://localhost:8080/api"
 
 export type FormData = {
     username: string,
@@ -7,12 +7,6 @@ export type FormData = {
 }
 
 export type Session = {
-    // private String id;
-    // private Instant startTime;
-    // private Instant endTime;
-
-    // private String category;
-
     id: string,
     startTime: string,
     endTime: string | null,
